dfe2ef2b05c9958792228ad5d63b7a718b8babd9
Veronika-Ivanova <71488973+Veronika-Ivanova@users.noreply.github.com>
2026-07-08 22:20:28 +0300

    upd datasets

--- src/datasets.py
+++ src/datasets.py
@@ -18,17 +18,6 @@ class CausalDataset(Dataset):
                  relevance_col, relevance_threshold,
                  user_col, item_col='item_id',
                  time_col='time_idx'):
-        """
-        Args:
-            df (pd.DataFrame): Dataframe with interactions.
-            max_length (int): Maximum sequence legth.
-            relevance_col (str): Relevance column in dataframe.
-            relevance_threshold (float): Threshold for negative feedback.
-            user_col (str): User column in dataframe.
-            item_col (str): Item column in dataframe.
-            time_col (str): Time column in dataframe.
-        """
-
         self.max_length = max_length
         self.user_col = user_col
         self.item_col = item_col
@@ -37,32 +26,28 @@ class CausalDataset(Dataset):
         self.relevance_threshold = relevance_threshold
         self._prepare_data(df)
 
-
     def _prepare_data(self, df):
-
         df = df.sort_values(self.time_col)
         self.items = df.groupby(self.user_col)[self.item_col].agg(list).to_dict()
         self.relevances = df.groupby(self.user_col)[self.relevance_col].agg(list).to_dict()
         self.user_ids = list(self.items.keys())
 
     def __len__(self):
-
         return len(self.items)
 
     def __getitem__(self, idx):
-        
         user_id = self.user_ids[idx]
         item_sequence = self.items[user_id]
         relevance_sequence = self.relevances[user_id]
-        
-        item_sequence = np.array(item_sequence).astype(float) 
+
+        item_sequence = np.array(item_sequence).astype(float)
         item_sequence = item_sequence[-self.max_length - 1:]
         relevance_sequence = relevance_sequence[-self.max_length - 1:]
-        
+
         input_ids = item_sequence[:-1].astype(int)
         labels = item_sequence[1:].astype(int)
         skip = (np.array(relevance_sequence)[:-1] < self.relevance_threshold).astype(int)
-        
+
         item = {'user_id': user_id, 'input_ids': input_ids,
                 'labels': labels, 'skip': skip}
 
@@ -75,23 +60,9 @@ class CausalPredictionDataset(CausalDataset):
     def __init__(self, df, max_length,
                  relevance_col, relevance_threshold,
                  user_col, item_col='item_id',
-                 time_col='time_idx', 
-                 validation_mode=False, validation_size=None, positive_eval=True):
-        """
-        Args:
-            df (pd.DataFrame): Dataframe with interactions.
-            max_length (int): Maximum sequence legth.
-            relevance_col (str): Relevance column in dataframe.
-            relevance_threshold (float): Threshold for negative feedback.
-            user_col (str): User column in dataframe.
-            item_col (str): Item column in dataframe.
-            time_col (str): Time column in dataframe.
-            validation_mode (bool): Validation or test prediction mode.
-            validation_size (int): Number of users to sample in validation set.
-                If None then take all users.
-            positive_eval (bool): use only positive items from user's history for elaluation.
-        """
-
+                 time_col='time_idx',
+                 validation_mode=False, validation_size=None, positive_eval=True,
+                 last_pos_targets=None, last_neg_targets=None):
         self.max_length = max_length
         self.user_col = user_col
         self.item_col = item_col
@@ -101,46 +72,67 @@ class CausalPredictionDataset(CausalDataset):
         self.validation_mode = validation_mode
         self.validation_size = validation_size
         self.positive_eval = positive_eval
+        self.target_pos = None
+        self.target_neg = None
 
-        users = df[self.user_col].unique()
+        if validation_mode:
+            if last_pos_targets is None or last_neg_targets is None:
+                raise ValueError(
+                    'last_pos_targets and last_neg_targets are required in validation_mode '
+                    '(e.g. last_pos_item_val and last_neg_item_val from prepare_splitted_data).'
+                )
+            self.target_pos, self.target_neg, users = self._prepare_targets(
+                last_pos_targets, last_neg_targets, user_col, item_col
+            )
+            df = df[df[user_col].isin(users)]
+            users = np.array(list(users))
+        else:
+            users = df[user_col].unique()
+
         if validation_size and (validation_size < len(users)):
             users = np.random.choice(users, size=validation_size, replace=False)
-            df = df[df[self.user_col].isin(users)]
+            df = df[df[user_col].isin(users)]
 
         self._prepare_data(df)
+        users_set = set(users)
+        self.user_ids = [u for u in self.user_ids if u in users_set]
+
+    def _prepare_targets(self, last_pos, last_neg, user_col, item_col):
+        pos = last_pos.set_index(user_col)[item_col].to_dict()
+        neg = last_neg.set_index(user_col)[item_col].to_dict()
+        users = set(pos.keys()) & set(neg.keys())
+        return pos, neg, users
 
     def __getitem__(self, idx):
-
         user_id = self.user_ids[idx]
-        item_sequence = self.items[user_id]
-        
-        item = {'user_id': user_id}
-        
+        item_sequence = np.array(self.items[user_id]).astype(float)
+        relevance_sequence = np.array(self.relevances[user_id])
+
+        item = {
+            'user_id': user_id,
+            'full_history': item_sequence.astype(int),
+        }
+
         if self.validation_mode:
-            item['target'] = item_sequence[-1]
-            item['full_history'] = item_sequence[:-1]
-            input_ids = np.array(item_sequence[:-1]).astype(float) 
-            item['target_rating'] = self.relevances[user_id][-1]
-            relevance_sequence = self.relevances[user_id][:-1]
-        else:
-            item['full_history'] = item_sequence
-            input_ids = np.array(item_sequence).astype(float)
-            relevance_sequence = self.relevances[user_id]
+            item['target_pos'] = int(self.target_pos[user_id])
+            item['target_neg'] = int(self.target_neg[user_id])
 
         if self.positive_eval:
-            positive_input_ids = input_ids[np.array(relevance_sequence) >= self.relevance_threshold].astype(int)
+            positive_input_ids = item_sequence[
+                relevance_sequence >= self.relevance_threshold
+            ].astype(int)
             item['input_ids'] = positive_input_ids[-self.max_length:]
         else:
             item['input_ids'] = item['full_history'][-self.max_length:]
 
-        return item     
-    
-    
+        return item
+
+
 class PaddingCollateFn:
     """Collate function for aggregating in batch."""
 
     def __init__(self, padding_value=0,
-                 labels_padding_value=-100, labels_keys=['labels'], 
+                 labels_padding_value=-100, labels_keys=['labels'],
                  add_aug_mask=False, add_negative_mask=False, bert=False):
 
         self.padding_value = padding_value
@@ -151,11 +143,9 @@ class PaddingCollateFn:
         self.bert = bert
 
     def __call__(self, batch):
-
         collated_batch = {}
 
         for key in batch[0].keys():
-
             if np.isscalar(batch[0][key]):
                 collated_batch[key] = torch.tensor([example[key] for example in batch])
                 continue
@@ -182,13 +172,13 @@ class PaddingCollateFn:
 
         if self.bert:
             attention_mask = collated_batch['input_ids'] != self.padding_value
-            collated_batch['attention_mask'] = attention_mask.to(dtype=torch.float32)  
+            collated_batch['attention_mask'] = attention_mask.to(dtype=torch.float32)
 
         return collated_batch
 
 
 class BarlowDataset(Dataset):
-    """Training dataset for negative feedback."""
+    """Training dataset for NegBT."""
 
     padding_value = 0
     labels_padding_value = -100
@@ -196,19 +186,7 @@ class BarlowDataset(Dataset):
     def __init__(self, df, max_length,
                  relevance_col, relevance_threshold,
                  user_col, item_col='item_id',
-                 time_col='time_idx', 
-                ):
-        """
-        Args:
-            df (pd.DataFrame): Dataframe with interactions.
-            max_length (int): Maximum sequence legth.
-            relevance_col (str): Relevance column in dataframe.
-            relevance_threshold (float): Threshold for negative feedback.
-            user_col (str): User column in dataframe.
-            item_col (str): Item column in dataframe.
-            time_col (str): Time column in dataframe.
-        """
-
+                 time_col='time_idx'):
         self.max_length = max_length
         self.user_col = user_col
         self.item_col = item_col
@@ -218,28 +196,25 @@ class BarlowDataset(Dataset):
         self._prepare_data(df)
 
     def _prepare_data(self, df):
-
         df = df.sort_values(self.time_col)
         self.items = df.groupby(self.user_col)[self.item_col].agg(list).to_dict()
         self.relevances = df.groupby(self.user_col)[self.relevance_col].agg(list).to_dict()
         self.user_ids = list(self.items.keys())
 
     def __len__(self):
-
         return len(self.items)
 
     def __getitem__(self, idx):
-        
         user_id = self.user_ids[idx]
         item_sequence = self.items[user_id]
-        relevance_sequence = self.relevances[user_id] 
-        item_sequence = np.array(item_sequence).astype(float) 
-                
+        relevance_sequence = self.relevances[user_id]
+        item_sequence = np.array(item_sequence).astype(float)
+
         positive_item_sequence = item_sequence[np.array(relevance_sequence) >= self.relevance_threshold]
         positive_item_sequence = positive_item_sequence[-self.max_length - 1:]
         positive_input_ids = positive_item_sequence[:-1].astype(int)
         positive_labels = positive_item_sequence[1:].astype(int)
-        
+
         last_positive_index = np.where(item_sequence == positive_input_ids[-1])[-1][-1]
         input_ids = item_sequence[:last_positive_index + 1][-self.max_length:].astype(int)
 
@@ -315,7 +290,7 @@ class BTSRDataset(Dataset):
 
 
 class CausalNegativeFeedbackDataset(Dataset):
-    """Training dataset for negative feedback."""
+    """Training dataset with separate positive and negative sequences."""
 
     padding_value = 0
     labels_padding_value = -100
@@ -323,19 +298,7 @@ class CausalNegativeFeedbackDataset(Dataset):
     def __init__(self, df, max_length,
                  relevance_col, relevance_threshold,
                  user_col, item_col='item_id',
-                 time_col='time_idx', 
-                ):
-        """
-        Args:
-            df (pd.DataFrame): Dataframe with interactions.
-            max_length (int): Maximum sequence legth.
-            relevance_col (str): Relevance column in dataframe.
-            relevance_threshold (float): Threshold for negative feedback.
-            user_col (str): User column in dataframe.
-            item_col (str): Item column in dataframe.
-            time_col (str): Time column in dataframe.
-        """
-
+                 time_col='time_idx'):
         self.max_length = max_length
         self.user_col = user_col
         self.item_col = item_col
@@ -345,24 +308,21 @@ class CausalNegativeFeedbackDataset(Dataset):
         self._prepare_data(df)
 
     def _prepare_data(self, df):
-
         df = df.sort_values(self.time_col)
         self.items = df.groupby(self.user_col)[self.item_col].agg(list).to_dict()
         self.relevances = df.groupby(self.user_col)[self.relevance_col].agg(list).to_dict()
         self.user_ids = list(self.items.keys())
 
     def __len__(self):
-
         return len(self.items)
 
     def __getitem__(self, idx):
-        
         user_id = self.user_ids[idx]
         item_sequence = self.items[user_id]
         relevance_sequence = self.relevances[user_id]
-        
-        item_sequence = np.array(item_sequence).astype(float) 
-        
+
+        item_sequence = np.array(item_sequence).astype(float)
+
         positive_item_sequence = item_sequence[np.array(relevance_sequence) >= self.relevance_threshold]
         negative_item_sequence = item_sequence[np.array(relevance_sequence) < self.relevance_threshold]
 
@@ -377,4 +337,4 @@ class CausalNegativeFeedbackDataset(Dataset):
         item = {'user_id': user_id, 'input_ids': positive_input_ids, 'negative_input_ids': negative_input_ids,
                 'labels': positive_labels, 'negative_labels': negative_labels}
 
-        return item    
+        return item
