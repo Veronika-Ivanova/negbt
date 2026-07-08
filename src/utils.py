c9cd91d3ae7b12231f79b883882f3b1596abf5a1
Veronika-Ivanova <71488973+Veronika-Ivanova@users.noreply.github.com>
2026-07-08 23:16:41 +0300

    upd utils

--- src/utils.py
+++ src/utils.py
@@ -38,3 +38,21 @@ def fix_seed(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.cuda.set_rng_state
+
+
+def get_csv_metrics_path(trainer, filename='metrics.csv'):
+    """Return path to CSV metrics file (PL 1.x and 2.x CSVLogger)."""
+    logger = trainer.logger
+    if isinstance(logger, (list, tuple)):
+        logger = logger[0]
+
+    if hasattr(logger, 'log_dir'):
+        log_dir = logger.log_dir
+        if not callable(log_dir):
+            return os.path.join(log_dir, filename)
+
+    experiment = logger.experiment
+    log_dir = experiment.log_dir
+    if callable(log_dir):
+        log_dir = log_dir()
+    return os.path.join(log_dir, filename)
